ad128084e038831d05fdf98d76ccb25af9267c5d
Huy Quynh <huyquynh11to@gmail.com>
2020-06-13 10:34:21 +0700

    Initial commit

---
+++ DAL_Hotel/DBConnect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace DAL_Hotel
+{
+
+
+    public class DBConnect
+    {
+         protected MySqlConnection con2 =  new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=testdb;");
+        //protected SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-B182N8G\MSSQLSERVER01;Initial Catalog=QLKS;Integrated Security=True");
+    }
+}
---
+++ Hotel_Management/GUI_TrangChu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel_Management
+{
+    public partial class GUI_TrangChu : Form
+    {
+        public GUI_TrangChu()
+        {
+            InitializeComponent();
+        }
+    }
+}
